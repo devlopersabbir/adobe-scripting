73a6afaaf3a06c1ba96088524b2f67d012bcc1f7
devlopersabbir <devlopersabbir@gmail.com>
2023-10-02 01:43:21 +0600

    ✨ Able to show number of page with loop

--- page_length.jsx
+++ page_length.jsx
@@ -1,14 +1,30 @@
 if (app && app.name === "Adobe Illustrator") {
-  const document = app.activeDocument;
-
-  if (document) {
-    // get number of pages
-    const pageLength = document.artboards.length;
-    for (let i = 0; i <= pageLength; i++) {
-      alert("the page is: " + document.artboards[i]);
+  try {
+    var document = app.activeDocument;
+    if (!document) {
+      alert("No document is open!");
     }
-  } else {
-    alert("No document is open!");
+    // get the number of pages
+    var pageLength = document.artboards.length;
+    for (var i = 0; i < pageLength; i++) {
+      var artboard = document.artboards[i];
+      var labelText = "Page " + (i + 1);
+
+      // create a new text frame
+      var textFrame = document.textFrames.add();
+      textFrame.contents = labelText;
+
+      // Position the text frame at the right top corner
+      textFrame.left = artboard.artboardRect[2] - 110; // Set to the right edge of the artboard
+      textFrame.top = artboard.artboardRect[1] - 30; // Adjust the vertical position as needed
+
+      textFrame.textRange.characterAttributes.size = 30; // Set the desired text size
+
+    }
+    app.redraw();
+    // alert("Page numbers added to the right top corner.");
+  } catch (error) {
+    alert("Error occurred: " + error);
   }
 } else {
   alert("Adobe Illustrator is not running.");
